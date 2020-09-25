be643baf92ed69693be71a405b47f561d89eb4c1
Sid Hudson <sid.hudson@my.simpson.edu>
2020-09-25 12:37:01 -0500

    Not done yet, close though

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -23,9 +23,24 @@ def main():
             done = True
             print("Thanks for playing!")
 
+        elif user_choice.upper() == "A":
+            if canteen >= 1:
+                canteen -= 1
+                thirst = 0
+
+            elif canteen == 1:
+                print("error")
+
+        elif user_choice.upper() == "B":
+            miles_traveled += random.randrange(5, 13)
+            print("You traveled")
+            thirst += 1
+            camel_tiredness += 1
+            native_travel += random.randrange(7, 15)
+
         elif user_choice.upper() == "C":
             miles_traveled += random.randrange(10, 21)
-            print("you traveled")
+            print("You traveled")
             thirst += 1
             camel_tiredness += random.randrange(1, 4)
             native_travel += random.randrange(7, 15)
@@ -39,5 +54,35 @@ def main():
             print("Miles traveled:  ", miles_traveled)
             print("Drinks in canteen:  ", canteen)
             print("The natives are ", miles_traveled - native_travel, " miles behind you.")
+
+        if thirst > 4:
+            print("You are thirsty")
+
+        elif thirst > 6:
+            print("You died of thirst!")
+            done = True
+
+        if camel_tiredness > 5:
+            print("Your camel is tired")
+
+        elif camel_tiredness > 8:
+            print("Your camel is dead!")
+            done = True
+
+        if native_travel == miles_traveled:
+            print("The natives have caught you!")
+            done = True
+
+        elif native_travel < 15:
+            print("The natives are getting close!")
+
+        if miles_traveled > 200:
+            print("You won!")
+            done = True
+
+        for i in range(20):
+            if random.randrange(5) == 0:
+                print("You have found an oasis!")
+
 
 main()
